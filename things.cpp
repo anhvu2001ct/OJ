c3f05b595a09b3b642ad1dccdc1fd9fa9bce4008
nganhvu <anhvu2001ct@gmail.com>
2019-07-24 17:18:36 +0700

    .

--- things.cpp
+++ things.cpp
@@ -1,14 +1,9 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<typename T, Cmp = less<T>>
+using oss = tree<T, null_type, Cmp, rb_tree_tag, tree_order_statistics_node_update>;
 
 char* sfm(const char *fmt, ...) {
 	static char res[100];va_list args;va_start(args, fmt);vsnprintf(res, 100, fmt, args);va_end(args);return res;
-}
-
-int main() {
-	#ifdef KITTENS
-		freopen("main.inp", "r", stdin);
-		freopen("main.out", "w", stdout);
-	#endif
-    return 0;
 }
