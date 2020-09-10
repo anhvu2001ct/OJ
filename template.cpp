ab3ee2f00afa0d2e453778be758264aabdaf24c5
AnhVu <nganhvu2161@gmail.com>
2020-09-11 05:47:43 +0700

    .

--- template.cpp
+++ template.cpp
@@ -27,7 +27,7 @@ template<class T, class Cmp = less<T>> using oss = tree<T, null_type, Cmp, rb_tr
 #define buga(a, s, e) cout << '{'; fto (i, s, e) cout << a[i] << " }"[i == e]; cout << endl
 
 template<class T1, class T2> ostream& operator<<(ostream &os, pair<T1, T2> const &v) {
-	return os << '(' << v.ff << ", " << v.ss << ')';
+    return os << '(' << v.ff << ", " << v.ss << ')';
 }
 
 template<typename T>
@@ -35,29 +35,29 @@ void _bug(ostream &os, T const &var) { os << var << endl; }
 
 template<typename T, typename... Args>
 void _bug(ostream &os, T const &var, Args const &... args) {
-	os << var << ' ';
-	_bug(os, args...);
+    os << var << ' ';
+    _bug(os, args...);
 }
 
 template<typename... Args>
 void _bugn(string const &s, Args const &... args) {
-	auto split = [](string const &s, string const &delim) {
-		int next, pos = 0;
-		vector<string> res;
-		while (1) {
-			next = s.find(delim, pos);
-			if (next == string::npos) break;
-			res.push_back(s.substr(pos, next-pos));
-			pos = next+sz(delim);
-		}
-		res.push_back(s.substr(pos));
-		return res;
-	};
+    auto split = [](string const &s, string const &delim) {
+        int next, pos = 0;
+        vector<string> res;
+        while (1) {
+            next = s.find(delim, pos);
+            if (next == string::npos) break;
+            res.push_back(s.substr(pos, next-pos));
+            pos = next+sz(delim);
+        }
+        res.push_back(s.substr(pos));
+        return res;
+    };
 
-	ostringstream os; _bug(os, args...);
-	string tmp = os.str(); tmp.pop_back(); 
-	vector<string> v1 = split(s, ", "), v2 = split(tmp, " ");
-	fto1 (i, 0, sz(v1)) cout << '[' << v1[i] << ": " << v2[i] << "]" << " \n"[i == sz(v1)-1];
+    ostringstream os; _bug(os, args...);
+    string tmp = os.str(); tmp.pop_back(); 
+    vector<string> v1 = split(s, ", "), v2 = split(tmp, " ");
+    fto1 (i, 0, sz(v1)) cout << '[' << v1[i] << ": " << v2[i] << "]" << " \n"[i == sz(v1)-1];
 }
 
 double const pi = acos(-1);
@@ -69,24 +69,24 @@ int const maxn = 1e5+3;
 
 #define multi_test 1
 void _main() {
-	
+    
 }
 
 int main() {
-	#ifdef KITTENS
-		freopen("main.inp", "r", stdin);
-		freopen("main.out", "w", stdout);
-	#endif
-	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    #ifdef KITTENS
+        freopen("main.inp", "r", stdin);
+        freopen("main.out", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-	int t = 1;
-	if (multi_test) cin >> t;
-	while (t--) {
-		_main();
-	}
+    int t = 1;
+    if (multi_test) cin >> t;
+    while (t--) {
+        _main();
+    }
 
-	#ifdef KITTENS
-		cerr << 0.001*clock() << endl;
-	#endif
-	return 0;
+    #ifdef KITTENS
+        cerr << 0.001*clock() << endl;
+    #endif
+    return 0;
 }
