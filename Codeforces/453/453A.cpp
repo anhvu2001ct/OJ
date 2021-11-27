469baba6d7259fb83b10d783227e51ab6ab01043
nganhvu <nganhvu2161@gmail.com>
2021-11-27 20:57:23 +0700

    .

--- Codeforces/453/453A.cpp
+++ Codeforces/453/453A.cpp
@@ -1,101 +1,71 @@
-#include <bits/stdc++.h>
+#ifdef _LOCAL
+	#include "local_include.hpp"
+#else
+	#include <bits/stdc++.h>
+	using namespace std;
+#endif
 #include <ext/pb_ds/assoc_container.hpp>
-
-using namespace std;
 using namespace __gnu_pbds;
 
 #define fto(i, s, e) for (int i = (s); i <= (e); ++i)
 #define fto1(i, s, e) for (int i = (s); i < (e); ++i)
 #define fdto(i, s, e) for (int i = (s); i >= (e); --i)
 #define fit(it, a) for (auto it = (a).begin(); it != (a).end(); ++it)
-#define fat(i, a) for (auto i : (a))
 
 #define ll long long
 #define ii pair<int, int>
 #define pll pair<ll, ll>
+#define eb emplace_back
 template<class T, class Cmp = less<T>> using oss = tree<T, null_type, Cmp, rb_tree_tag, tree_order_statistics_node_update>;
 
 #define bc __builtin_popcountll
-#define y1 ansdj
 #define endl '\n'
-#define ff first
-#define ss second
 #define sz(v) int((v).size())
 #define all(v) (v).begin(), (v).end()
-#define bug(...) _bug(cout, __VA_ARGS__)
-#define bugn(...) _bugn(#__VA_ARGS__, __VA_ARGS__)
-#define buga(a, s, e) cout << '{'; if (e < s) cout << '}'; else fto (__i, s, e) cout << a[__i] << " }"[__i == e]; cout << endl
-
-template<class T1, class T2> ostream& operator<<(ostream &os, pair<T1, T2> const &v) {
-    return os << '(' << v.ff << ", " << v.ss << ')';
-}
+#define buga(a, s, e) fto(__i, s, e) cout << a[__i] << " \n"[__i == e];
+#define bugan(a, s, e) fto(__i, s, e) cout << a[__i] << endl;
 
 template<typename T>
-void _bug(ostream &os, T const &var) { os << var << endl; }
+void bug(T const &var) { cout << var << endl; }
 
 template<typename T, typename... Args>
-void _bug(ostream &os, T const &var, Args const &... args) {
-    os << var << ' ';
-    _bug(os, args...);
-}
-
-template<typename... Args>
-void _bugn(string const &s, Args const &... args) {
-    auto split = [](string const &s, string const &delim) {
-        int next, pos = 0;
-        vector<string> res;
-        while (1) {
-            next = s.find(delim, pos);
-            if (next == string::npos) break;
-            res.push_back(s.substr(pos, next-pos));
-            pos = next+sz(delim);
-        }
-        res.push_back(s.substr(pos));
-        return res;
-    };
-
-    ostringstream os; _bug(os, args...);
-    string tmp = os.str(); tmp.pop_back(); 
-    vector<string> v1 = split(s, ", "), v2 = split(tmp, " ");
-    fto1 (i, 0, sz(v1)) cout << '[' << v1[i] << ": " << v2[i] << "]" << " \n"[i == sz(v1)-1];
+void bug(T const &var, Args const &... args) {
+	cout << var << ' ';
+	bug(args...);
 }
 
 double const pi = acos(-1);
 #define oo 1000000007
 #define OO 1000000000000000003LL
-int const maxn = 1e5+3;
+int mod = oo;
+int const maxn = 2e5+3;
 
-
+int n, m;
 
 #define multi_test 0
 void _main() {
-    int n, m;
-    cin >> m >> n;
-    double a = 0, ans = 0;
-    fdto (i, m, 1) {
-        double b = 1 - pow(1.0*(i-1)/m, n);
-        ans += (b - a)*i;
-        a = b;
-    }
-    cout << fixed << setprecision(9);
-    bug(ans);
+	cin >> n >> m;
+	double ans = 0;
+	fto (j, 1, n) {
+		double p = pow(1.0*j/n, m) - pow(1.0*(j-1)/n, m);
+		ans += p*j;
+	}
+	cout << setprecision(5);
+	bug(ans);
 }
 
 int main() {
-    #ifdef KITTENS
-        freopen("main.inp", "r", stdin);
-        freopen("main.out", "w", stdout);
-    #endif
-    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	#ifdef _LOCAL
+		freopen("main.inp", "r", stdin);
+		freopen("main.out", "w", stdout);
+	#endif
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-    int t = 1;
-    if (multi_test) cin >> t;
-    while (t--) {
-        _main();
-    }
+	int t = 1; if (multi_test) cin >> t;
+	while (t--) _main();
 
-    #ifdef KITTENS
-        cerr << 0.001*clock() << endl;
-    #endif
-    return 0;
+	#ifdef _LOCAL
+		bugt;
+	#endif
+	return 0;
 }
